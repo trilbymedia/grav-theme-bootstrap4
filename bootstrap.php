623a52016297077235c62b6681218039de4b201b
Jakub <jakub@rockettheme.com>
2018-05-23 15:40:37 +0200

    Init commit

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Bootstrap extends Theme
+{
+    // Boostrap plugin will look for this class var to know it should load
+    public $load_bootstrapper_plugin = true;
+
+}
