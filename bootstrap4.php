a41eaa48faa566ebbec84f5a9613901573dce172
Jakub <jakub@rockettheme.com>
2018-05-24 21:08:39 +0200

    Changes and modifications

--- bootstrap4.php
+++ bootstrap4.php
@@ -3,7 +3,7 @@ namespace Grav\Theme;
 
 use Grav\Common\Theme;
 
-class Bootstrap extends Theme
+class Bootstrap4 extends Theme
 {
     // Boostrap plugin will look for this class var to know it should load
     public $load_bootstrapper_plugin = true;
