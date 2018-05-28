1ef1ab91bbf7219c49589b55279a4dc15a638260
Andy Miller <rhuk@mac.com>
2018-05-28 13:43:13 -0600

    added customizable header

--- bootstrap4.php
+++ bootstrap4.php
@@ -8,4 +8,16 @@ class Bootstrap4 extends Theme
     // Boostrap plugin will look for this class var to know it should load
     public $load_bootstrapper_plugin = true;
 
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onThemeInitialized'    => ['onThemeInitialized', 0]
+        ];
+    }
+
+    public function onThemeInitialized()
+    {
+        $this->config->set('plugins.bootstrapper.version', 'v4');
+    }
+
 }
